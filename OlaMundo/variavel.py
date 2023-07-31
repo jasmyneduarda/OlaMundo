96c91dbe3e1e31ba95a0f0bf5db4365e56483bad
Jasmyn Eduarda <jasmyn.sabino@aluno.senai.br>
2023-07-31 11:13:37 -0300

    criado o arquivo variavel.py

---
+++ OlaMundo/variavel.py
@@ -0,0 +1,11 @@
+# input é uma função que permite perguntar algo ao usuário
+nome = input("Qual é o seu nome?")
+
+'''
+Bloco
+de 
+comentário
+permite que várias linhas se tornem um cometário
+'''
+
+print("Olá,", nome,". Tudo bem com você?")
