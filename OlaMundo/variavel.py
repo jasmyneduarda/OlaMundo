fb9b23ded22acf9ccc208d9483ce7af8b91b266e
Jasmyn Eduarda <jasmyn.sabino@aluno.senai.br>
2023-08-01 09:21:06 -0300

    alteração com os tipos

--- OlaMundo/variavel.py
+++ OlaMundo/variavel.py
@@ -1,12 +1,79 @@
 # input é uma função que permite perguntar algo ao usuário
-nome = input("Qual é o seu nome?")
-
+#nome = input("Qual é o seu nome?")
 '''
 Bloco
 de 
 comentário
 permite que várias linhas se tornem um cometário
 '''
+#print("Olá,", nome,". Tudo bem com você?")
 
-print("Olá,", nome,". Tudo bem com você?")
-print("Alteração depois de clonado")
+#print(type(nome))
+a = 10
+b = 5.8
+c = "Rio de Janeiro"
+d = True
+
+print("a: ", a)
+print("b: ", b)
+print("c: ", c)
+print("d: ", d)
+
+print("Tipo da var a: ", type(a)) # tipo inteiro (numeros inteiros)
+print("Tipo da var b: ", type(b)) # tipo float (numeros reais)
+print("Tipo da var c: ", type(c)) # tipo string (alfanumérico)
+print("Tipo da var d: ", type(d)) # tipo boolean (TRue or False)
+
+a = 20
+print("a: ", a)
+b = "São Paulo"
+print("b: ", b)
+print("Tipo da var a: ", type(a))
+print("Tipo da var b: ", type(b))
+
+a = input("Imforme um número: ")
+b = input("Imforme um número: ")
+print("a: ", a, " -b: ", b)
+print("Tipo da var a: ", type(a))
+print("Tipo da var b: ", type(b))
+c = a + b # concatenou as strings de a e b
+print("c: ", c)
+print("Tipo da var c: ", type(c))
+d = int(a)
+print("d: ", d)
+print("Tipo da var d: ", type(d))
+
+a = int(input("Imforme um número: "))
+b = int(input("Imforme um número: "))
+print("a: ", a, " -b: ", b)
+print("Tipo da var a: ", type(a))
+print("Tipo da var b: ", type(b))
+c = a + b
+print("c: ", c)
+print("Tipo da var c: ", type(c))
+
+a = float(input("Imforme um número: "))
+b = float(input("Imforme um número: "))
+print("a: ", a, " -b: ", b)
+print("Tipo da var a: ", type(a))
+print("Tipo da var b: ", type(b))
+c = a + b
+print("c: ", c)
+print("Tipo da var c: ", type(c))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
